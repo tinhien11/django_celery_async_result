7b316bd563b4fc8600c5676351fcab6ab39c43b3
ccbn <thanhtin@siliconstraits.vn>
2016-05-29 00:10:42 +0700

    easy runing with docker-compose, update req

--- django_celery_async_result/settings.py
+++ django_celery_async_result/settings.py
@@ -15,7 +15,7 @@ import djcelery
 djcelery.setup_loader()
 
 # Celery config
-BROKER_URL = 'redis://localhost:6379/0'
+BROKER_URL = 'redis://redis/0'
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
