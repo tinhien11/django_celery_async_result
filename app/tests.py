87223ed8f4529799c555b4d542b5864c7622adc9
ccbn <thanhtin@siliconstraits.vn>
2016-05-29 10:33:40 +0700

    add unit test for task

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.test.utils import override_settings
+from app.tasks import add
 
-# Create your tests here.
+
+class AddTestCase(TestCase):
+    @override_settings(
+        CELERY_EAGER_PROPAGATES_EXCEPTIONS=True,
+        CELERY_ALWAYS_EAGER=True,
+        BROKER_BACKEND='memory')
+    def test_add_task(self):
+        result = add.delay('1', '2')
+        self.assertTrue(result.successful())
+        self.assertEqual(result.result, '12', "Should return 12 on add string 1 and 2")
