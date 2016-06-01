49e891f22448b0d36f394b9beabe2b01cac90170
ccbn <thanhtin@siliconstraits.vn>
2016-06-01 14:12:39 +0700

    complete spider fshare with urllib

--- spider/fshare_spider_urllib.py
+++ spider/fshare_spider_urllib.py
@@ -1,12 +1,7 @@
 # -*- coding: utf-8 -*-
 import urllib
 import urllib2
-import glob
-import sqlite3
-import os
 import cookielib
-import json
-import time
 import lxml.html
 
 LOGIN_EMAIL = 'thanhtinpk092007@gmail.com'
@@ -36,12 +31,18 @@ def login_cookies():
     data = parse_form(html)
     data['LoginForm[email]'] = LOGIN_EMAIL
     data['LoginForm[password]'] = LOGIN_PASSWORD
-    del data['yt0'] # remove unicode unneed item for encode
+    del data['yt0']  # remove unicode unneed item for encode
     encoded_data = urllib.urlencode(data)
     request = urllib2.Request(LOGIN_URL, encoded_data)
-    response = opener.open(request)
-    print response.geturl()
+    opener.open(request)
     return opener
+
+
+def get_download_url(opener, url_to_get):
+    request = urllib2.Request(url_to_get)
+    request.get_method = lambda: 'HEAD'
+    response = opener.open(request)
+    return response
 
 
 def parse_form(html):
@@ -56,7 +57,11 @@ def parse_form(html):
 
 
 def main():
-    login_cookies()
+    opener = login_cookies()
+    response = get_download_url(opener, "https://www.fshare.vn/file/FFNQHWVDW6VN/")
+    print response.geturl()
+    print response.code
+    print response.info()
 
 
 if __name__ == '__main__':
