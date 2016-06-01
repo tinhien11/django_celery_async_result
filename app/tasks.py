f68c0acee342a6a0206890547fe4cf8286c70ce7
ccbn <thanhtin@siliconstraits.vn>
2016-06-01 14:39:30 +0700

    handle submit url fshare and response download link, bootstrap template

--- app/tasks.py
+++ app/tasks.py
@@ -1,6 +1,9 @@
+from spider.fshare_spider_urllib import login_cookies, get_download_url
 from celery import task
 
 
 @task()
-def add(x, y):
-    return x + y
+def task_get_link_fshare(fshare_url):
+    opener = login_cookies()
+    response = get_download_url(opener, fshare_url)
+    return response.geturl()
--- app/views.py
+++ app/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
-from tasks import add
+from tasks import task_get_link_fshare
 from celery.result import AsyncResult
 
 
 def create_task(request):
     if request.method == 'POST':
-        task = add.delay(request.POST['x'], request.POST['y'])
+        task = task_get_link_fshare.delay(request.POST.get('fshare_url'))
         return redirect('task_result', task_id=task.task_id)
     return render(request, 'create_task.html', {})
 
