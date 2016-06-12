1a660335dbf546cc02423100c777df5484889a00
ccbn <thanhtin@siliconstraits.vn>
2016-06-12 13:10:58 +0700

    catch exception on get elm html

--- spider/vnpost_spider.py
+++ spider/vnpost_spider.py
@@ -11,37 +11,44 @@ def normalize(html):
     tree = lxml.html.fromstring(html)
 
     overview = {}
-    parcel_id_elm = tree.find_class('col-sm-3 package-code')[0].getchildren()
-    parcel_id = parcel_id_elm[2].text_content().strip()
-    overview['parcel_id'] = parcel_id
 
-    parcel_weight_elm = tree.find_class('col-sm-4 package-weight')[0].getchildren()
-    parcel_weight = parcel_weight_elm[2].text_content().strip()
-    overview['parcel_weight'] = parcel_weight
+    try:
+        parcel_id_elm = tree.find_class('col-sm-3 package-code')[0].getchildren()
+        parcel_id = parcel_id_elm[2].text_content().strip()
+        overview['parcel_id'] = parcel_id
 
-    package_status_elm = tree.find_class('col-sm-4 package-location')[0].getchildren()
-    parcel_status = package_status_elm[2].text_content().strip()
-    overview['parcel_status'] = parcel_status
+        parcel_weight_elm = tree.find_class('col-sm-4 package-weight')[0].getchildren()
+        parcel_weight = parcel_weight_elm[2].text_content().strip()
+        overview['parcel_weight'] = parcel_weight
 
-    tracking_info_elm = tree.find_class('table-tracking-info')[0].getchildren()
-    country_elm = tracking_info_elm[0]
-    from_country = country_elm[1].text_content()
-    to_country = country_elm[3].text_content()
-    overview['from_country'] = from_country
-    overview['from to_country'] = to_country
+        package_status_elm = tree.find_class('col-sm-4 package-location')[0].getchildren()
+        parcel_status = package_status_elm[2].text_content().strip()
+        overview['parcel_status'] = parcel_status
+
+        tracking_info_elm = tree.find_class('table-tracking-info')[0].getchildren()
+        country_elm = tracking_info_elm[0]
+        from_country = country_elm[1].text_content()
+        to_country = country_elm[3].text_content()
+        overview['from_country'] = from_country
+        overview['from to_country'] = to_country
+    except Exception as error:
+        pass
 
     details = []
-    list_detail_event = tree.find_class('timeline-list-item')[0].getchildren()[0].getchildren()
-    for e in list_detail_event:
-        event_dict = {}
-        list_event_elm = e.getchildren()
-        event_time = list_event_elm[0].text_content()
-        event_dict['event_time'] = event_time
-        event_name = list_event_elm[1].text_content()
-        event_dict['event_name'] = event_name
-        event_localtion = list_event_elm[1].getchildren()[0].text_content()
-        event_dict['event_localtion'] = event_localtion
-        details.append(event_dict)
+    try:
+        list_detail_event = tree.find_class('timeline-list-item')[0].getchildren()[0].getchildren()
+        for e in list_detail_event:
+            event_dict = {}
+            list_event_elm = e.getchildren()
+            event_time = list_event_elm[0].text_content()
+            event_dict['event_time'] = event_time
+            event_name = list_event_elm[1].text_content()
+            event_dict['event_name'] = event_name
+            event_localtion = list_event_elm[1].getchildren()[0].text_content()
+            event_dict['event_localtion'] = event_localtion
+            details.append(event_dict)
+    except Exception as error:
+        pass
 
     res = {}
     res['overview'] = overview
